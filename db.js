6ef7252f6b2c864c6650e0a367a3aa9b987e2148
Divyanshi <divyanshisrivas1804@gmail.com>
2026-05-29 00:49:19 +0530

    fix db connection

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection(process.env.MYSQL_URL || {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
 });
 
 connection.connect(function(err) {
